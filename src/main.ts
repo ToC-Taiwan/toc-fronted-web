b4277b1433e111c2bb90de0af7bcf097c0d577e5
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2024-04-04 10:57:38 +0800

    refactor: "Integrate Vuetify directly into main application"
    - Added `materialdesignicons.css` and `vuetify/styles` imports to `main.scss`
    - Removed the `registerPlugins` function from `main.ts` and directly imported `createVuetify` instead
    - Initialized `vuetify` in `main.ts` and added it to the app using `app.use(vuetify)`
    - Deleted the `plugins/index.ts` file, which previously handled the registration of plugins
    - Removed the `plugins/vuetify.ts` file, its contents were moved to other files

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import "./assets/css/main.scss";
 
 import { setupI18n } from "@/i18n";
-import { registerPlugins } from "@/plugins";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
+import { createVuetify } from "vuetify";
 
 import App from "@/App.vue";
 import router from "@/router";
 
 const app = createApp(App);
+const vuetify = createVuetify({});
 const i18n = setupI18n();
 
-registerPlugins(app);
-
 app.use(createPinia());
+app.use(vuetify);
 app.use(router);
 app.use(i18n);
 
