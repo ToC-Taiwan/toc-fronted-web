44255159a868133f0fe1ac5a2323c8b983c21e62
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2024-04-30 21:24:15 +0800

    feat: "Integrate Firebase and VueFire into Vue app"
    - Added Firebase environment variables to the GitHub workflow file
    - Added Firebase variables to the `.template.env` file
    - Added `firebase` and `vuefire` as new dependencies in `package.json`
    - Updated `vitest` version in `package.json`
    - Created new file `firebase.ts` for Firebase app initialization
    - Imported Firebase app initialization in `main.ts` and added `VueFire` to the Vue app setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import App from "@/App.vue";
 import "@/assets/css/styles.scss";
+import { firebaseApp } from "@/firebase";
 import { i18n } from "@/i18n";
 import router from "@/router";
 import { registerLicense } from "@syncfusion/ej2-base";
@@ -8,10 +9,12 @@ import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
 import ToastService from "primevue/toastservice";
 import { createApp } from "vue";
+import { VueFire } from "vuefire";
 
 registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE);
 createApp(App)
   .use(createPinia())
+  .use(VueFire, { firebaseApp })
   .use(ChartPlugin)
   .use(router)
   .use(i18n)
