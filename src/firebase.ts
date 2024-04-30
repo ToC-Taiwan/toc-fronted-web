1b263b40a785ead57b6c3748c741a6fdd5a43842
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2024-04-30 21:40:44 +0800

    feat: "Implement Firebase Analytics Initialization"
    - Import `initializeAnalytics` from `firebase/analytics`
    - Change `firebaseApp` from a constant export to a named export
    - Initialize Firebase Analytics with `firebaseApp` after its creation

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
+import { initializeAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 
-export const firebaseApp = initializeApp({
+const firebaseApp = initializeApp({
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
@@ -10,3 +11,7 @@ export const firebaseApp = initializeApp({
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 });
+
+initializeAnalytics(firebaseApp);
+
+export { firebaseApp };
